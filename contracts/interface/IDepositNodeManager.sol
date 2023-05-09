5f4752fcd85f2da3b9ae547df2f6c0fdfa5a51aa
lei.lv@okg.com <lei.lv@okg.com>
2023-05-09 16:22:27 +0800

    更新IDepositNodeManager合约接口

--- contracts/interface/IDepositNodeManager.sol
+++ contracts/interface/IDepositNodeManager.sol
@@ -11,12 +11,20 @@ interface IDepositNodeManager {
      */
     event NodeOperatorRegistered(address indexed operator, address indexed nodeAddress);
     /**
-     * @notice Emit when validators registered
-     * @param nodeAddress Contract address of node operator
-     * @param startIndex The first index of validators registered
-     * @param count Count of validators registered
+     * @notice Emit when validator pubkey and signature added
+     * @param validatorId Validator index
+     * @param operator Operator address
+     * @param pubkey Validator public key
      */
-    event NodeValidatorsRegistered(address indexed nodeAddress, uint256 startIndex, uint256 count);
+    event SigningKeyAdded(uint256 indexed validatorId, address indexed operator, bytes indexed pubkey);
+
+    /**
+     * @notice Emit when validator activated
+     * @param validatorId Validator index
+     * @param operator Operator address
+     * @param pubkey Validator public key
+     */
+    event SigningKeyActivated(uint256 indexed validatorId, address indexed operator, bytes indexed pubkey);
 
     /**
      * @notice Validator status, should be WAITING_ACTIVATED -> VALIDATING -> EXITING -> EXITED
@@ -28,7 +36,8 @@ interface IDepositNodeManager {
         VALIDATING,
         EXITING,
         SLASHING,
-        EXITED
+        EXITED,
+        UNSAFE
     }
 
     /**
@@ -46,34 +55,36 @@ interface IDepositNodeManager {
     function getNodeOperator(address operator) external view returns (address nodeAddress, bool isActive);
 
     /**
-     * @notice Get available validator pubkeys count
-     * @return Available validator pubkeys count
-     * Only validators signed pre-exit msg can really get funds and be activated
-     */
-    function getAvailableValidatorsCount() external view returns (uint256);
-
-    /**
      * @notice Node operators register validators and get operator index in return
      * @dev Function called when operators add pubkeys
      * @param operator Node operator address, for access control
-     * @param count Node operator count request to register
-     * @return The first index of validators registered
+     * @param pubkey public key
+     * @return The index of validator registered
      */
-    function registerValidators(address operator, uint256 count) external returns (uint256);
+    function registerValidator(address operator, bytes calldata pubkey) external returns (uint256);
 
     /**
      * @notice Get contract and status of validator by index
-     * @param validatorIndex Index of validator
-     * @return nodeAddress Node operator contract address the validator belongs to
+     * @param index Index of validator
+     * @return operator Operator address the validator belongs to
+     * @return pubkey Public key
      * @return status Validator status
      */
-    function getNodeValidator(
-        uint256 validatorIndex
-    ) external view returns (address nodeAddress, ValidatorStatus status);
+    function getNodeValidator(uint256 index) external view returns (address operator, bytes memory pubkey, ValidatorStatus status);
 
     /// @notice Set minimum deposit amount, may be changed by DAO
     function setMinOperatorStakingAmount(uint256 minAmount) external;
 
     /// @notice Get minimum deposit amount, may be changed by DAO
     function getMinOperatorStakingAmount() external view returns (uint256);
+
+    /**
+     * @notice Activate validators by index  map(index, operator)
+     * @param indexes Index array of validators
+     */
+    function activateValidators(uint256[] calldata indexes) external;
+
+    function setValidatorUnsafe(uint256 index, uint256 slashAmount) external;
+
+    function distributeNodeOperatorRewards(uint256 pethAmount) external;
 }
