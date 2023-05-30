6e404818f165954245aac6bb3d6031cdd7ce0d03
rengui.zhu <rengui.zhu@okg.com>
2023-05-30 17:29:12 +0800

    完善核心合约测试脚本

--- scripts/core/test-core.js
+++ scripts/core/test-core.js
@@ -30,13 +30,22 @@ async function main() {
   const x = await dawnDeposit.getWithdrawalCredentials();
   console.log("WithdrawalCredentials:" , x);
 
-  console.log('0x2bA0f16Ee6C18d5bca0ba140588077C9fF606fbD balance: ', await dawnDeposit.balanceOf('0x2bA0f16Ee6C18d5bca0ba140588077C9fF606fbD'));
+  console.log('user 0x2bA0f16Ee6C18d5bca0ba140588077C9fF606fbD balance: ', await dawnDeposit.balanceOf('0x2bA0f16Ee6C18d5bca0ba140588077C9fF606fbD'));
+  //DawnInsurance：0x32a3fD7024B4ed67De1d331091f0075DF0243bDd
+  console.log('DawnInsurance balance: ', await dawnDeposit.balanceOf('0x32a3fD7024B4ed67De1d331091f0075DF0243bDd'));
+  // DawnTreasury： 0xe00c3897596983A9457B0a5D36059A6Cc210d2BC
+  console.log('DawnTreasury balance: ', await dawnDeposit.balanceOf('0xe00c3897596983A9457B0a5D36059A6Cc210d2BC'));
+  // DepositNodeManager: 0x1E4f4fd4513dCE5FdD51e7e00c9ea0Ca093986cD
+  console.log('DepositNodeManager balance: ', await dawnDeposit.balanceOf('0x1E4f4fd4513dCE5FdD51e7e00c9ea0Ca093986cD'));
+  // DepositNodeOperator：0x2acd68AF4211BC82Ca526BE28e2fF3A1cfb424c5
+  console.log('DepositNodeOperator balance: ', await dawnDeposit.balanceOf('0x2acd68AF4211BC82Ca526BE28e2fF3A1cfb424c5'));
+
   //getTotalPooledEther、getBufferedEther
   console.log('TotalPooledEther', await dawnDeposit.getTotalPooledEther());
   console.log('BufferedEther', await dawnDeposit.getBufferedEther());
   //getEtherByPEth、getPEthByEther
-  console.log('PEth : Eth = 1 : ', await dawnDeposit.getEtherByPEth(1));
-  console.log('Eth : PEth = 1 : ', await dawnDeposit.getPEthByEther(1));
+  console.log('PEth : Eth = 1994408201304753028 : ', await dawnDeposit.getEtherByPEth('1994408201304753028'));
+  console.log('Eth : PEth = 4999999999999999 : ', await dawnDeposit.getPEthByEther(4999999999999999));
   //     bytes32 internal constant _PRE_DEPOSIT_VALIDATORS_KEY = keccak256("dawnDeposit.preDepositValidators");
   console.log('pre deposit validators: ', await dawnStorage.getUint(keccak256("dawnDeposit.preDepositValidators")));
   // getBeaconStat
@@ -50,6 +59,9 @@ async function main() {
   console.log('insurance fee: ', await dawnStorage.getUint(keccak256("dawnDeposit.insuranceFee")));
   console.log('treasury fee: ', await dawnStorage.getUint(keccak256("dawnDeposit.treasuryFee")));
   console.log('node operator fee: ', await dawnStorage.getUint(keccak256("dawnDeposit.nodeOperatorFee")));
+
+  //await dawnStorage.setAddress(keccak256(encodePacked('contract.address', 'DepositContract')), '0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b');
+  console.log('DepositContract: ', await dawnStorage.getAddress(keccak256(encodePacked('contract.address', 'DepositContract'))));
 
 }
 
