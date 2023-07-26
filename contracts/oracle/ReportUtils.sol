3a526dc1c54c003c617d2115e73c13adfd5d97a4
houlei.huo <houlei.huo@okg.com>
2023-07-26 18:32:23 +0800

    fix:合约格式化问题修复

--- contracts/oracle/ReportUtils.sol
+++ contracts/oracle/ReportUtils.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 library ReportUtils {
-    uint256 internal constant COUNT_OUTMASK = 0xFFFFFFFFFFFFFFFFFFFFFFFF0000;
+    uint256 internal constant _COUNT_OUTMASK = 0xFFFFFFFFFFFFFFFFFFFFFFFF0000;
 
     function encode(uint64 beaconBalance, uint32 beaconValidators) internal pure returns (uint256) {
         return (uint256(beaconBalance) << 48) | (uint256(beaconValidators) << 16);
@@ -23,7 +23,7 @@ library ReportUtils {
 
     /// @notice Check if the given reports are different, not considering the counter of the first
     function isDifferent(uint256 value, uint256 that) internal pure returns (bool) {
-        return (value & COUNT_OUTMASK) != that;
+        return (value & _COUNT_OUTMASK) != that;
     }
 
     function getCount(uint256 value) internal pure returns (uint16) {
